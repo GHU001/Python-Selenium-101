176f26c0dbffdbf1a0ed08a893658af2dba82af8
GHU001 <ghuu001@gmail.com>
2020-05-25 23:23:43 +0800

    update webauto

--- webauto.py
+++ webauto.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
+import time
 
 wd = webdriver.Chrome(r'/Users/guanyanghu/Downloads/chromedriver')
+wd.implicitly_wait(5)
+
 wd.get('http://www.google.com/')
 
-
+time.sleep(10)
+element = wd.find_element_by_class_name('gLFyf')
+element.send_keys('  \n')
+print(element)
 
 
 
